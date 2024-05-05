3d2518502bdb1d461be0d44c3a66885c33a1afbd
LauraKirby <laurakirby26@gmail.com>
2024-05-04 17:22:36 -0700

    add openai

--- src/api/controllers/questions.controller.ts
+++ src/api/controllers/questions.controller.ts
@@ -1,17 +1,39 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
+import OpenAI from "openai";
 
 export const GET = async (req: Request, res: Response) => {
   res.render("index", { firstName: "Ari" });
 };
 
 export const POST = async (req: Request, res: Response) => {
-  console.log("ask OpenAOI a question");
-  console.log("Received question from client:", req.body);
-  const response = {
-    question: req.body.question,
-    answer: "hire a trainer"
+  const question = req.body?.question;
+  let answer;
+  if (!question) res.json({ error: "Start by asking a question" });
+
+  try {
+    const openai = new OpenAI({
+      apiKey: process.env["OPENAI_API_KEY"],
+    });
+
+    const q = "dog behavior problems";
+    answer = await openai.chat.completions.create({
+      messages: [{ role: "user", content: question }],
+      model: "gpt-3.5-turbo-16k",
+    });
+
+    console.log("\n\n\n\n");
+    console.log(answer);
+    console.log("\n\n\n\n");
+
+    if (answer?.error) {
+      answer = answer.error.message;
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ question, error: "something went wrong" });
   }
-  res.json(response);
+
+  res.json({ question, answer });
 };
 
 export const QuestionsController = {
