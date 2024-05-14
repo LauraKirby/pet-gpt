c71f026cfb3c1e7552c8b8b4c608d815e63559e6
LauraKirby <laurakirby26@gmail.com>
2024-05-14 18:26:20 -0500

    update config to what we actually use

--- src/api/config.ts
+++ src/api/config.ts
@@ -9,32 +9,18 @@ dotenv.config({
   }`,
 });
 
-const ORIGINS = process.env.VITE_FRONTEND_ALLOWED_ORIGINS
-  ? process.env.VITE_FRONTEND_ALLOWED_ORIGINS
-  : process.env.FRONTEND_ALLOWED_ORIGINS
-  ? process.env.FRONTEND_ALLOWED_ORIGINS
-  : "[]";
-const FRONTEND_ALLOWED_ORIGINS = JSON.parse(ORIGINS);
-const COOKIE_DOMAIN = process.env.VITE_COOKIE_DOMAIN
-  ? process.env.VITE_COOKIE_DOMAIN
-  : process.env.COOKIE_DOMAIN
-  ? process.env.COOKIE_DOMAIN
-  : "localhost";
+const FRONTEND_ALLOWED_ORIGINS = JSON.parse(process.env.FRONTEND_ALLOWED_ORIGINS || "");
+
 const PORT = process.env.PORT ?? 4000;
 
-const TOKEN_EXPIRATION_IN_SECONDS = 60 * 60 * 24 * 7;
 if (!FRONTEND_ALLOWED_ORIGINS) {
   throw new Error(
     "Missing FRONTEND_ALLOWED_ORIGINS env value, should be an array of strings"
   );
 }
-if (!COOKIE_DOMAIN) {
-  throw new Error("Missing COOKIE_DOMAIN env value.");
-}
 
 export {
-  COOKIE_DOMAIN,
   FRONTEND_ALLOWED_ORIGINS,
-  PORT,
-  TOKEN_EXPIRATION_IN_SECONDS,
+  PORT
 };
+
